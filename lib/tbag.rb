e8a73443c746d4cc6558192972ee76106b0a6bf7
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-07-17 13:31:46 -0400

    Update tbag.rb

--- lib/tbag.rb
+++ lib/tbag.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Benjamin S. Meyers
+# Copyright (C) 2015 Benjamin S. Meyers as Lion Logic
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
